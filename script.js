502f1c32f526a9cdc9b227b56a7ba669d22c78e4
Rohan Sharma <rohansh2017@gmail.com>
2024-05-20 16:15:23 -0400

    Added clear grid functionality.
    Added another button that allows the user to clear the grid without retyping.

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
 const defaultSize = 16;
 const grid = document.querySelector("#grid");
+let size = defaultSize;
 
-function getSize(){
+function getSize() {
   let size = Number(prompt("Enter a number greater than 0 and under 100"));
-  while (size > 100 || isNaN(size)){
-    size = Number(prompt("You did not enter a number smaller than 100, or you entered a negative number. Please enter a number greater than 0 and under 100"));
+  while (size > 100 || isNaN(size)) {
+    size = Number(
+      prompt(
+        "You did not enter a number smaller than 100, or you entered a negative number. Please enter a number greater than 0 and under 100"
+      )
+    );
   }
   return Math.round(size);
 }
 
 function createGrid(gridSize) {
-    height = grid.clientHeight / gridSize;
-    width = grid.clientWidth / gridSize;
-    for(let i = 0; i < gridSize**2; i++) {
-      const pixel = document.createElement("div");
-      pixel.classList.add("pixel");
-      pixel.setAttribute("style", `height: ${height}px; width: ${width}px;`)
-      pixel.addEventListener("mouseover", () => {
-        pixel.style.backgroundColor = "black";
-      });
-      grid.appendChild(pixel);
-    }
+  height = grid.clientHeight / gridSize;
+  width = grid.clientWidth / gridSize;
+  for (let i = 0; i < gridSize ** 2; i++) {
+    const pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    pixel.setAttribute("style", `height: ${height}px; width: ${width}px;`);
+    pixel.addEventListener("mouseover", () => {
+      pixel.style.backgroundColor = "black";
+    });
+    grid.appendChild(pixel);
   }
+  size = gridSize;
+}
+
+function clearGrid(){
+
+}
 
 createGrid(defaultSize);
 
 const btn = document.querySelector("#btn");
 btn.addEventListener("click", () => {
-    let size = getSize();
-    grid.replaceChildren();
-    createGrid(size);
+  size = getSize();
+  grid.replaceChildren();
+  createGrid(size);
+});
+
+const btn2 = document.querySelector("#clear-grid");
+btn2.addEventListener("click", () => {
+  grid.replaceChildren();
+  createGrid(size);
 });
